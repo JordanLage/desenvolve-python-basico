70bcd0c5ed7a5f1e0b1b8f3227f1df164b743e73
Jordan Lage <129227688+JordanLage@users.noreply.github.com>
2024-10-04 20:08:53 -0300

    Exercícios aula 2 tipos de dados

---
+++ Modulo 2/aula2_questao2.py
@@ -0,0 +1,14 @@
+# 2) Transforme o seguinte algoritmo em código. Lembre-se das funções print e type que já conhecemos de forma geral.
+# Armazene o texto "o resultado é"  em uma variável, o valor 10 em outra variável, e o valor 3.5 numa terceira variável.
+# Some os valores da segunda e terceira variável e armazene em outra variável.
+# Imprima todas as variáveis na ordem de criação e imprima também seus tipos.
+
+exibir_resultado = "o resultado é:"
+num1 = 10
+num2 = 3.5
+soma = num1 + num2
+
+print(type(exibir_resultado))
+print(type(num1))
+print(type(num2))
+print(type(soma))
